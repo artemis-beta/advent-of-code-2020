c13c17f2d39d261ace9a24a6bf43ce1de5da6fb9
Kristian Zarebski <krizar312@gmail.com>
2020-12-08 17:35:01 +0000

    Completed Day 8

--- advent_of_code/day_8/__init__.py
+++ advent_of_code/day_8/__init__.py
@@ -17,23 +17,47 @@ RANDOM_CMDS = ['Configuring', 'Updating', 'Verifying options for process',
                'Checking runtime latency for process', 'Finding libraries from process']
 RANDOM_VARS = ['dhoc', 'lbfort', 'imgf', 'dsk2', 'xsfc', 'hmd0', 'apr-cr', 'ts3']
 
-def run_lines(script_lines: List[str], test:bool = False) -> int:
+def run_lines(script_lines: List[str], test:bool = False, quiet: bool = False) -> int:
+    '''
+    Run system using the given configuration
+
+    Arguments
+    ---------
+
+    script_lines        lines of boot startup script
+    
+    Optional Arguments
+    ------------------
+
+    test                run in unit test mode
+    quiet               turn off printout statements
+
+    Returns
+    -------
+
+    If successful, returns the final value of the allocator
+    
+    '''
     accumulator = 0
 
     _current_line = 0
     
-    print(f"[{colored('info', 'blue')}] Reading config lines")
-    print(f"[ {colored('ok', 'green')} ] Determining accumulator value")
+    if not quiet:
+        print(f"[{colored('info', 'blue')}] Reading config lines")
+        print(f"[ {colored('ok', 'green')} ] Determining accumulator value")
 
     _visited_lines = []
 
     while True:
         if _current_line in _visited_lines:
-            print(f"[{colored('FAIL', 'red')}] Infinite boot sequence ... aborting")
-            print(f"[....] accumulator returned {accumulator}")
+            if not quiet:
+                print(f"[{colored('FAIL', 'red')}] Infinite boot sequence ... aborting")
+                print(f"[....] accumulator returned {accumulator}")
             if test:
                 return accumulator
-            exit()
+            raise RuntimeError(f"Entering infinite loop")
+        if _current_line >= len(script_lines):
+            return accumulator
         _visited_lines.append(_current_line)
         if test:
             print(f"[{colored('TEST', 'yellow')}] LINE {_current_line}: {script_lines[_current_line].strip()}")
@@ -42,26 +66,93 @@ def run_lines(script_lines: List[str], test:bool = False) -> int:
             _current_line += 1
             if cmd == 'acc':
                 accumulator += int(arg.replace('+', ''))
-                print(f"[....] Adjusting, acc = {accumulator}")
+                if not quiet:
+                    print(f"[....] Adjusting, acc = {accumulator}")
             else:
-                print(f"[ {colored('ok', 'green')} ] {choice(RANDOM_CMDS)} {choice(RANDOM_VARS)}")
+                if not quiet:
+                    print(f"[ {colored('ok', 'green')} ] {choice(RANDOM_CMDS)} {choice(RANDOM_VARS)}")
         elif cmd == 'jmp':
             _current_line += int(arg.replace('+', ''))
         else:
-            print(f"[{colored('FAIL', 'red')}] Command '{cmd}' not recognised ... boot failed!")
+            if not quiet:
+                print(f"[{colored('FAIL', 'red')}] Command '{cmd}' not recognised ... boot failed!")
             exit()
         
     return accumulator
 
-def run_boot_sequence(boot_config: Optional[str] = BOOT_SCRIPT) -> None:
+def run_boot_sequence(boot_config: str) -> None:
+    '''
+    Run the startup procedure using a given boot script
+
+    Arguments
+    ---------
+
+    boot_config         script from which to perform boot procedure. Uses 
+
+    '''
     if not os.path.exists(boot_config):
         raise FileNotFoundError(f"Could not locate file '{boot_config}'")
 
     print(f"[ {colored('ok', 'green')} ] Locating bootup config")
     with open(boot_config) as f:
-        _accumulator = run_lines(f.readlines())
+        try:
+            _accumulator = run_lines(f.readlines())
+        except RuntimeError:
+            return
     print(f"[ {colored('ok', 'green')} ] Accumulator set to value {_accumulator}")
 
+def repair_boot_config(boot_script_lines: List[str]) -> int:
+    '''
+    Attempt a repair of the boot config script by switching jmp <-> nop statements
+
+    Arguments
+    ---------
+
+    boot_script_lines       boot command lines from a run script
+
+    '''
+    _accumulator = None
+    _lines_copy = [m for m in boot_script_lines]
+    for i, line in enumerate(boot_script_lines):
+        if 'jmp' in line:
+            _lines_copy[i] = _lines_copy[i].replace('jmp', 'nop')
+        elif 'nop' in line:
+            _lines_copy[i] = _lines_copy[i].replace('nop', 'jmp')
+        try:
+            _accumulator = run_lines(_lines_copy, quiet=True)
+            break
+        except RuntimeError:
+            _lines_copy = [m for m in boot_script_lines]
+            continue
+    return _accumulator
+
+def fix_boot_sequence(boot_config: Optional[str] = BOOT_SCRIPT) -> None:
+    '''
+    Attempt a fix on the bootloader
+
+    Optional Arguments
+    ------------------
+
+    boot_config             option to supply custom boot script, default will
+                            repair existing configuration
+
+    '''
+    print(f"\nAttempting system repair...\n")
+    if not os.path.exists(boot_config):
+        raise FileNotFoundError(f"Could not locate file '{boot_config}'")
+
+    _accumulator = repair_boot_config(open(boot_config).readlines())
+
+    with open(boot_config) as f:
+        _lines = f.readlines()
+
+    if not _accumulator:
+        print("Error: System boot could not be repaired, shutting down...")
+        exit()
+    print("Success: System boot repaired, rebooting...\n")
+    _accumulator = run_lines(_lines_copy)
+    print(f"[ {colored('ok', 'green')} ] Accumulator set to value {_accumulator}")
 
 if __name__ in "__main__":
     run_boot_sequence()
+    fix_boot_sequence()
--- tests/test_day_8.py
+++ tests/test_day_8.py
@@ -1,10 +1,17 @@
 import pytest
 import os
-from advent_of_code.day_8 import run_lines
+from advent_of_code.day_8 import run_lines, repair_boot_config
 
+
+@pytest.fixture
+def boot_config():
+    DATA_INPUT = os.path.join(os.path.dirname(__file__), 'day_8.dat')
+    return open(DATA_INPUT).readlines()
 
 @pytest.mark.day8
-def test_bootup():
-    DATA_INPUT = os.path.join(os.path.dirname(__file__), 'day_8.dat')
+def test_bootup(boot_config):
+    assert run_lines(boot_config, test=True) == 5
 
-    assert run_lines(open(DATA_INPUT).readlines(), test=True) == 5
+@pytest.mark.day8
+def test_boot_repair(boot_config):
+    assert repair_boot_config(boot_config) == 8
