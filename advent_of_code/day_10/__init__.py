e29376b4d6a95131cc3fba3353ec620652f4a4e5
Kristian Zarebski <krizar312@gmail.com>
2020-12-12 00:33:55 +0000

    Completed day 10

--- advent_of_code/day_10/__init__.py
+++ advent_of_code/day_10/__init__.py
@@ -8,30 +8,36 @@
 #############################################################################################
 
 import os
+import re
 from typing import List, Tuple, Dict
 from functools import reduce
 from scipy.stats import binom
 from operator import mul
 
 
-def get_adapter_ordering(
-        joltage_list: List[int],
-        start_joltage: int = 0,
-        permitted_range_for_adaptor: List[int] = range(1, 4)) -> List[int]:
-
-    _ordered_adapters = sorted(joltage_list)
-
-
-def get_builtin_joltage(joltage_list: List[int]) -> int:
-    return max(joltage_list) + 3
-
-
 def get_prod_jolt_sums(data_input: List[str], device_joltage_diff = 3) -> int:
+    '''
+    Get the product of all the sums of joltages
+
+    Arguments
+    ---------
+
+    data_input              list of adapter joltages as file lines
+    (device_joltage_diff)   joltage difference between device and highest
+                            adapter rating
+
+    Returns
+    -------
+
+    Calculates the product of the number of 1-3 joltage differences
+    between adapter ratings
+
+    '''
+
     _data = [int(i.strip()) for i in data_input]
     _ordered = sorted(_data)
 
     _count_dict: Dict[int, int] = {}
-
 
     for i in range(len(_ordered)-1):
         _diff = _ordered[i+1]-_ordered[i]
@@ -44,11 +50,107 @@ def get_prod_jolt_sums(data_input: List[str], device_joltage_diff = 3) -> int:
 
     return reduce(mul, _count_dict.values(), 1)
 
+
+def tribonacci(n: int) -> int:
+    '''
+    Returns the nth term of the tribonacci sequence: 0, 0, 1, 1, 2, 4, ...
+    '''
+    if n in [0, 1]:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3)
+
+def get_consecutives_n_possibilities(nums: List[int]) -> int:
+    '''
+    Find the number of possible paths for the given number list
+
+    Arguments
+    ---------
+
+    nums        list of numbers with max separation of 3
+
+    Returns
+    -------
+
+    Total number of possible paths
+
+    '''
+
+    # Get all intervals between numbers
+    _intervals = [j-i for i, j in list(zip(nums[:-1], nums[1:]))]
+
+    # If there are any intervals of 2, then there are two possible routes
+    # between groups so x2 on product, e.g.:
+    # 
+    #   [1, 2, 3, 5, 6]
+    #
+    #   3 -> 5, 3 -> 6 are both permitted
+    #
+    _fac_n2_interv = 2*_intervals.count(2) if _intervals.count(2) > 0 else 1
+    
+    # Split the interval list into groups of interval=1 by splitting by
+    # any appearance of 3 or 2
+    _group_sizes: List[str] = re.split('3|2', ''.join(map(str, _intervals)))
+
+    # Get the number of integers in each group, remembering that
+    # size_group = interval_size + 1
+    _group_sizes_int: List[int] = [len(i)+1 for i in _group_sizes if i]
+
+    # Take the produce of the Tribonacci sequence terms remembering that
+    # the sequence is offset by 1 in this case (starts at 1 not 0)
+    _product = reduce(mul, [tribonacci(i+1) for i in _group_sizes_int], 1)
+
+    # Return produce applying count of interval 2 factor
+    return _product*_fac_n2_interv
+
 def get_n_distinct(data_input: List[str], permitted_intervals=[1,2,3]) -> int:
+    '''
+    Find the number of possible paths for the numbers in a given data file
+
+    Arguments
+    ---------
+
+    data_input              data file containing list of adapters as integers
+
+    (permitted_intervals)   allowed joltage differences between consecutive
+                            adapter ratings
+
+    Returns
+    -------
+
+    Total number of possible paths
+
+    '''
     _data = sorted(int(i.strip()) for i in data_input)
+    _con = 0
+
+    # Start adapter can be any value within permitted intervals if available
+    # so need to add all of these cases
+    for i in permitted_intervals:
+        if i in _data:
+            _index_i = _data.index(i)
+            _con += get_consecutives_n_possibilities(_data[_index_i:])
+    return _con
 
 
 def process_file(data_input: str, device_joltage_diff: int) -> int:
+    '''
+    Arguments
+    ---------
+
+    data_input              file containing list of adapter joltage ratings
+    (device_joltage_diff)   joltage difference between device and highest
+                            adapter rating
+
+    Returns
+    -------
+
+    Calculates the product of the number of 1-3 joltage differences
+    between adapter ratings
+
+    '''
     if not os.path.exists(data_input):
         raise FileNotFoundError(f"Could not find data file '{data_input}'")
 
